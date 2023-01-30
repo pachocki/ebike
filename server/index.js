247b35e51d25de1894c380ab85c4406cce16714d
Wojciech Pachocki <wojtek.kodehode@gmail.com>
2023-01-30 20:21:36 +0100

    updts

--- server/index.js
+++ server/index.js
@@ -20,11 +20,7 @@ mongoose
     console.log(err);
   });
 
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
